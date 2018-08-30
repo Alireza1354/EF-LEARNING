1b8f02da6722832b703e7e7c3704e3a4573fb7e0
Karimi <karimifromlfco@gmail.com>
2018-08-30 16:11:17 +0430

    Add project files.

---
+++ LEARNING_EF_CODE_FIRST/Models/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Models
+{
+	public class DatabaseContext : System.Data.Entity.DbContext
+	{
+
+        // Static Constructors(C# Programming Guide)
+        // A static constructor does not take access modifiers or have parameters.
+        // A static constructor is called automatically to initialize the class before the
+        // first instance is created or any static members are referenced.
+        // A static constructor cannot be called directly.
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/static-constructors
+        static DatabaseContext()
+		{
+		}
+
+		public DatabaseContext() : base()
+		{
+		}
+
+		public System.Data.Entity.DbSet<Person> People { get; set; }
+	}
+}
